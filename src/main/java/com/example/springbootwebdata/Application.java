2b84ce61a1afa980b1a49d362d70a9e2bb4c1937
Gyowanny Queiroz <gyowanny.queiroz@sky.uk>
2017-09-21 21:10:56 +0100

    Adds CORS configuration

--- src/main/java/com/example/springbootwebdata/Application.java
+++ src/main/java/com/example/springbootwebdata/Application.java
@@ -2,11 +2,25 @@ package com.example.springbootwebdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //registry.addMapping("/healthmetrics").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
